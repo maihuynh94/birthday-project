15b377bc4cdc9fd7d0dc7753124ce30df4b5532b
MaiHuynh94 <maihthuynh@gmail.com>
2020-05-12 22:54:11 +0700

    birthday

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("openBtn").addEventListener("click", function(){
+    console.log(123);
+    
+    document.getElementById("card").style.display = "block";
+});
+document.getElementById("closeBtn").addEventListener("click", function(){
+    console.log(123);
+    
+    document.getElementById("card").style.display = "none";
+});
+
